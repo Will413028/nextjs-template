fb880091ee3741c76953cc45b33aea1f74fda6a4
will <will413028@gmail.com>
2024-06-04 23:13:12 +0800

    ✨ Add image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div>
-      <Link href='/'>Home</Link>
-      <Link href='/performance'>Performance</Link>
-      <Link href='/reliability'>Reliability</Link>
-      <Link href='/scale'>Scale</Link>
+    <div className='absolute z-10 w-full text-white'>
+      <nav className='contaienr relative mx-auto flex flex-wrap items-center justify-between p-8'>
+        <Link href='/' className='font-bond text-3xl'>
+          Home
+        </Link>
+
+        <div className='space-x-4 text-xl'>
+          <Link href='/performance'>Performance</Link>
+          <Link href='/reliability'>Reliability</Link>
+          <Link href='/scale'>Scale</Link>
+        </div>
+      </nav>
     </div>
   );
 }
