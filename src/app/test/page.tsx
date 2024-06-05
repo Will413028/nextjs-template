24e3bc8ddb68fafbe9e655d5112b1fb0ccfdb0ab
will <will413028@gmail.com>
2024-06-06 01:09:34 +0800

    ✨ Get users

---
+++ src/app/test/page.tsx
@@ -0,0 +1,27 @@
+interface User {
+  userId: string;
+  firstname: string;
+}
+
+interface Users {
+  "Templating example": string;
+  users: Array<User>;
+  id: number;
+  name: string;
+}
+
+export default async function User() {
+  const response = await fetch("http://localhost:3001/users");
+
+  const res = await response.json();
+
+  const renderedUsers = res.users.map((user: User) => (
+    <li key={user.userId}>{user.firstname}</li>
+  ));
+
+  return (
+    <div>
+      <ul>{renderedUsers}</ul>
+    </div>
+  );
+}
