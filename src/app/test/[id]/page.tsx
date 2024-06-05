2913b2ee7b4659e996c214a5a61afae67033c49a
will <will413028@gmail.com>
2024-06-06 01:19:38 +0800

    ✨ Add test/:id

---
+++ src/app/test/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ShowPage(props: any) {
+  // { params: { id: '1' }, searchParams: {} }
+  console.log(props);
+  return (
+    <div>
+      <h1>Show Page</h1>
+    </div>
+  );
+}
